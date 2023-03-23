0ef9860918594c639cdc0c226fffdcc72bf76022
rica213 <25f5k252l@mozmail.com>
2023-03-23 03:22:24 +0300

    Implement the toggle display of the modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,33 +1,52 @@
-import React from 'react';
+import { useDispatch } from 'react-redux';
+import { closeModal, searchByName } from '../redux/modal/modalSlice';
 import styles from '../styles/Modal.module.css';
 
-const Modal = () => (
-  <div className={styles.overlay}>
-    <span className={styles.closeBtn}>&times;</span>
-    <div className={styles.modal}>
-      <fieldset>
-        <legend>Search by:</legend>
-        <div>
-          <label htmlFor="symbol">
-            <input
-              type="radio"
-              id="symbol"
-              name="searchBy"
-              value="symbol"
-              checked
-            />
-            Symbol
-          </label>
-        </div>
-        <div>
-          <label htmlFor="name">
-            <input type="radio" id="name" name="searchBy" value="name" />
-            Name
-          </label>
-        </div>
-      </fieldset>
+const Modal = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className={styles.overlay}>
+      <span
+        className={styles.closeBtn}
+        onClick={() => dispatch(closeModal())}
+        onKeyDown={() => dispatch(closeModal())}
+        role="button"
+        tabIndex="0"
+      >
+        &times;
+      </span>
+      <div className={styles.modal}>
+        <fieldset>
+          <legend>Search by:</legend>
+          <div>
+            <label htmlFor="symbol">
+              <input
+                type="radio"
+                id="symbol"
+                name="searchBy"
+                value="symbol"
+                onChange={(e) => dispatch(searchByName(e.target.value))}
+              />
+              Symbol
+            </label>
+          </div>
+          <div>
+            <label htmlFor="name">
+              <input
+                type="radio"
+                id="name"
+                name="searchBy"
+                value="name"
+                onChange={(e) => dispatch(searchByName(e.target.value))}
+              />
+              Name
+            </label>
+          </div>
+        </fieldset>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Modal;
