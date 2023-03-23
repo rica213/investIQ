66fa265ac9797fcfa4ca4474b0a44bcaac97d18f
rica213 <25f5k252l@mozmail.com>
2023-03-23 03:32:20 +0300

    Formating the page

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./components/App";
-import "./styles/index.css";
-import store from "./redux/store";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './components/App';
+import './styles/index.css';
+import store from './redux/store';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Provider store={store}>
         <App />
       </Provider>
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
