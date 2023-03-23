2014f40edc8955892c55efe5d802a9ff64d9ba02
rica213 <25f5k252l@mozmail.com>
2023-03-23 03:14:19 +0300

    Update the list of endpoints

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,6 +1,5 @@
-export const baseUrl = "https://financialmodelingprep.com/api/v3/";
-export const searchByEndpoint = "search?query=";
-export const profileEndpoint = "profile/";
-export const greaterThanBillionEndPoint =
-  "stock-screener?marketCapMoreThan=10000000000&exchange=NASDAQ,NYSE";
-export const apikey = "d97b2f08a9a36a89179abfa4fb580330";
+export const baseUrl = 'https://financialmodelingprep.com/api/v3/';
+export const searchByEndpoint = 'search?query=';
+export const profileEndpoint = 'profile/';
+export const greaterThanTenBillionEndPoint = 'stock-screener?marketCapMoreThan=10000000000&exchange=NASDAQ,NYSE';
+export const apikey = 'd97b2f08a9a36a89179abfa4fb580330';
